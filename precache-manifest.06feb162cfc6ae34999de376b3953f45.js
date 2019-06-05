69b79d1659a467ca7e622919134493d70533edca
Martin Snyder <github@martinsnyder.net>
2019-06-05 13:59:30 -0400

    Initial Upload

---
+++ precache-manifest.06feb162cfc6ae34999de376b3953f45.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1091378864bc414c6ed00232a9948932",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70d3cfe6fb700254cb9d",
+    "url": "/static/css/main.38dd9947.chunk.css"
+  },
+  {
+    "revision": "642f282ff2b0333af90e",
+    "url": "/static/js/2.7b89ebfe.chunk.js"
+  },
+  {
+    "revision": "70d3cfe6fb700254cb9d",
+    "url": "/static/js/main.744a153f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0c5cb18585adbad7770a9fca2969c3ad",
+    "url": "/static/media/close.0c5cb185.svg"
+  },
+  {
+    "revision": "629e607f9b167adf47014111e259fd94",
+    "url": "/static/media/empty.629e607f.svg"
+  },
+  {
+    "revision": "167b03c59b2e676545ffb6fb8f874219",
+    "url": "/static/media/gavel.167b03c5.svg"
+  },
+  {
+    "revision": "eef0f891420c5756dfe2e32bb1753096",
+    "url": "/static/media/help.eef0f891.svg"
+  },
+  {
+    "revision": "b5fc000f1d3bd96f08d08c96178a1f18",
+    "url": "/static/media/money.b5fc000f.svg"
+  },
+  {
+    "revision": "167411111f4b670d6e449009087f9e52",
+    "url": "/static/media/no.16741111.svg"
+  },
+  {
+    "revision": "0c7fb2e282db98ecc51a876ca1071884",
+    "url": "/static/media/prison.0c7fb2e2.svg"
+  },
+  {
+    "revision": "4b8396167177561b82c4b846953eb924",
+    "url": "/static/media/scroll.4b839616.svg"
+  },
+  {
+    "revision": "8951dd1eff4e3c19f9a26a86e472274b",
+    "url": "/static/media/share.8951dd1e.svg"
+  },
+  {
+    "revision": "839598a33d3ab5c0792c838944339458",
+    "url": "/static/media/team.839598a3.svg"
+  },
+  {
+    "revision": "39c8a96a41c34f17c6a8f314b7d7cc7a",
+    "url": "/static/media/yes.39c8a96a.svg"
+  }
+]);
